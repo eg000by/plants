6c538d3a21515f71ebc497e5a3ae532cabfe2c4e
RNBochkov <bochkovnekit@gmail.com>
2023-10-31 22:16:52 +0400

    Added the acordion function

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function handleMouseMove(event, card) {
   const degreeY = (centerX - event.clientX) / 30;
   card.style.transform = `perspective(500px) rotateX(${degreeX}deg) rotateY(${degreeY}deg)`;
 }
-  // Функция закрытия окна при клике на крестик
-  function closeModal() {
-    document.querySelector('.overlay').style.display = 'none';
-  }
+// Функция закрытия окна при клике на крестик
+function closeModal() {
+  document.querySelector('.overlay').style.display = 'none';
+}
 function resetCard(card) {
   card.style.transform = 'none';
 }
@@ -19,95 +19,90 @@ const plantData = [
   {
     name: "Филодендрон сканденс Бразил",
     description: "Филодендрон сканденс Бразил - компактное растение с зелеными и желтыми листьями, идеальное для подвесных горшков. Легкий уход и яркий акцент в интерьере.",
-    price:`10`,
+    price: `10`,
+    image: "content/plantPhotos/plant1.png",
     characteristics: {
       height: "30 см",
-
       care: "Легкий",
+      wateringLow: `Поливайте его 2 раза в неделю`,
+      wateringMore: `Этот цветочек любит чтобы его поливали, но не сильно не терпила просто`,
     },
-    image: "content/plantPhotos/plant1.png"
-  },
-  {
-    name: "Фикус Ирен",
-    image: "content/plantPhotos/plant1.png",
-    description: "Фикус Ирен - ыыыыыыы",
-    characteristics: {
-      height: "35 см",
 
-      care: "Легкий"
-    }
+
   },
-  {
-    name: "Монстера Манки",
-    description: "Монстера Манки - ыыыыыыы",
-    image: "content/plantPhotos/plant1.png",
-    characteristics: {
-      height: "35 см",
-  
-      care: "Легкий"
-    }
-  },
-  {
-    name: "Эпипремнум Хэппи Лиф",
-    description: "Эпипремнум Хэппи Лиф - ыыыыыыы",
-    image: "content/plantPhotos/plant1.png",
-    characteristics: {
-      height: "35 см",
-   
-      care: "Легкий"
-    }
-  },
+
   // Добавьте данные для других растений
 ];
 
 
-  // Функция открытия окна при клике на карточку
-  function openModal(card) {
-    const modal = document.querySelector(".overlay");
-    const modalContent = modal.querySelector(".modal");
-    const leftSection = modalContent.querySelector(".left-section");
-    const rightSection = modalContent.querySelector(".right-section");
-  
-    // Очищаем левую и правую части модального окна
-    leftSection.innerHTML = "";
-    rightSection.innerHTML = "";
-  
-    // Извлекаем название растения на русском
-    const ruName = card.querySelector(".ru-name").textContent.trim();
-  
-    // Ищем в массиве объектов наше растение
-    const selectedPlant = plantData.find(plant => plant.name === ruName);
-  
-    // Проверяем, что растение найдено
-    if (selectedPlant) {
-      const imgSrc = selectedPlant.image;
-      const description = selectedPlant.description;
-      const characteristics = selectedPlant.characteristics;
-  
-      // Создаем элемент изображения и добавляем его в левую часть модального окна
-      const img = document.createElement("img");
-      img.src = imgSrc;
-      img.alt = ruName;
-      leftSection.appendChild(img);
-  
-      // Добавляем описание растения в правую часть модального окна
-      rightSection.insertAdjacentHTML(
-        "beforeend",
-        `<h2>${ruName}</h2>
+// Функция открытия окна при клике на карточку
+function openModal(card) {
+  const modal = document.querySelector(".overlay");
+  const modalContent = modal.querySelector(".modal");
+  const leftSection = modalContent.querySelector(".left-section");
+  const rightSection = modalContent.querySelector(".right-section");
+
+  // Очищаем левую и правую части модального окна
+  leftSection.innerHTML = "";
+  rightSection.innerHTML = "";
+
+  // Извлекаем название растения на русском
+  const ruName = card.querySelector(".ru-name").textContent.trim();
+
+  // Ищем в массиве объектов наше растение
+  const selectedPlant = plantData.find(plant => plant.name === ruName);
+
+  // Проверяем, что растение найдено
+  if (selectedPlant) {
+    const imgSrc = selectedPlant.image;
+    const description = selectedPlant.description;
+    const characteristics = selectedPlant.characteristics;
+
+    // Создаем элемент изображения и добавляем его в левую часть модального окна
+    const img = document.createElement("img");
+    img.src = imgSrc;
+    img.alt = ruName;
+    leftSection.appendChild(img);
+
+    // Добавляем описание растения в правую часть модального окна
+    rightSection.insertAdjacentHTML(
+      "beforeend",
+      `<h2>${ruName}</h2>
         <p>${description}</p>
         <div class="pricePlant"> Цена: ${selectedPlant.price}</div>
         <h3>Характеристики:</h3>
         <ul>
           <li>Высота: ${characteristics.height}</li>
           <li>Уход: ${characteristics.care}</li>
-        </ul>`
-      );
-  
-      // Открываем модальное окно
-      modal.style.display = "flex";
-    }
+        </ul>
+        <div class="accordion"> 
+          <details>
+            <summary>
+              <p>${characteristics.wateringLow}</p>
+            </summary>
+            <div class="accordioSummary">
+              <p>${characteristics.wateringMore}</p>
+            </div>
+          </details>
+          <details>
+            <summary>
+              <p>${characteristics.wateringLow}</p>
+            </summary>
+            <div class="accordioSummary">
+              <p>${characteristics.wateringMore}</p>
+            </div>
+          </details>
+        </div>`
+    );
+
+    // Открываем модальное окно
+    modal.style.display = "flex";
   }
-  // Функция закрытия окна при клике на крестик
+}
+
+
+
+// Функция закрытия окна при клике на крестик
 function closeModal() {
   document.querySelector('.overlay').style.display = 'none';
 }
@@ -156,4 +151,4 @@ function handleTouchEnd(event, card) {
   }
 }
 
- 
+
